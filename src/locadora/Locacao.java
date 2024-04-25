46471d756e41f1d7a18ca9ce98dea4538f31e74e
silva3265 <silva3265@gmail.com>
2024-04-25 12:47:47 -0300

    Rigidez do código com herança

---
+++ src/locadora/Locacao.java
@@ -0,0 +1,17 @@
+package locadora;
+
+public abstract class Locacao {
+
+    private Notebook notebook;
+
+    public Locacao(Notebook notebook) {
+        this.notebook = notebook;
+    }
+
+    public Notebook getNotebook() {
+        return notebook;
+    }
+
+    public abstract double calcularValorDevido(int horasUtilizadas);
+
+}
